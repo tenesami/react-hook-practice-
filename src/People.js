acbefe7189c28893b48bb13d50eae1b99bade0c7
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-01-17 13:51:21 -0700

    passing data using object

--- src/People.js
+++ src/People.js
@@ -2,24 +2,27 @@ import React, {useState} from 'react';
 import Form from './Form';
 
 function People() {
-    const [name, setName] = useState("")
-    const [age, setAge] = useState(21)
+    const [person, setPerson] = useState({
+        name: "",
+        age: 21
+    })
 
     const getYounger =() => {
-        setAge( age -1)
+        setPerson({...person, age: person.age -1})
     }
 
     const changeName = (data) => {
         console.log(data)
-        setName(data)
+        setPerson({...person, name: data})
     }
 
     return (
         
         <div>
             <Form handleOnSubmit={changeName} />
-            <h2>Hello my Name is {name}</h2>
-            <h4>And I am {age} Years Old</h4>
+            <h2>Hello my Name is {person.name}</h2>
+            
+            <h4>And I am {person.age} Years Old</h4>
              <button onClick={getYounger}>Get Younder</button>
         </div>
     )
