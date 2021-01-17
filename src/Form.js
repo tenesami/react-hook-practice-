2f4bbeaa678b01d288daaaa52daae156931547aa
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-01-17 13:35:05 -0700

    passing value using state

--- src/Form.js
+++ src/Form.js
@@ -1,14 +1,26 @@
 import React, {useState} from 'react'
 
 
-const Form = () => {
+const Form = (props) => {
 
  const [name, setName] = useState("")
+
+
+ const handleInput = (e) => {
+     //console.log(e.target.value)
+     setName(e.target.value)
+ }
+
+ const handleSubmit = (e) => {
+    e.preventDefault()
+    props.handleOnSubmit(name)
+    setName("")
+ }
  
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Name: </label>
-            <input type="text" value={name}></input>
+            <input type="text" value={name} onChange={handleInput}></input>
             <input type="submit" name="name"></input>
         </form>
                   
--- src/People.js
+++ src/People.js
@@ -2,22 +2,23 @@ import React, {useState} from 'react';
 import Form from './Form';
 
 function People() {
-    const [name, setName] = useState("Tesf")
+    const [name, setName] = useState("")
     const [age, setAge] = useState(21)
 
     const getYounger =() => {
         setAge( age -1)
     }
 
-    const toggleName =() => {
-        name === "Tesf" ? setName("Alem") : setName("Tesf")
+    const changeName = (data) => {
+        console.log(data)
+        setName(data)
     }
 
     return (
         
         <div>
+            <Form handleOnSubmit={changeName} />
             <h2>Hello my Name is {name}</h2>
-           <button onClick={toggleName}>Change Name</button>
             <h4>And I am {age} Years Old</h4>
              <button onClick={getYounger}>Get Younder</button>
         </div>
