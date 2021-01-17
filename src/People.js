d3ee71722c4863bc62e7de22f5ab85743f951388
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-01-16 23:14:44 -0700

    add decreamet and toggle name functinality

--- src/People.js
+++ src/People.js
@@ -2,13 +2,23 @@ import React, {useState} from 'react'
 
 
 function People() {
-    const [name, setName] = useState("Tesfaye")
+    const [name, setName] = useState("Tesf")
     const [age, setAge] = useState(21)
+
+    const getYounger =() => {
+        setAge( age -1)
+    }
+
+    const toggleName =() => {
+        name === "Tesf" ? setName("Alem") : setName("Tesf")
+    }
 
     return (
         <div>
             <h2>Hello my Name is {name}</h2>
+           <button onClick={toggleName}>Change Name</button>
             <h4>And I am {age} Years Old</h4>
+             <button onClick={getYounger}>Get Younder</button>
         </div>
     )
 }
